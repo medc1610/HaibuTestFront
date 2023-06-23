27b92f51a9d8621377c0e181da8adfcd5b53c1d7
medc1610 <medc1610@gmail.com>
2023-06-22 22:53:25 -0400

    mi primer commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  dataUrl: 'https://my-json-server.typicode.com/HaibuSolutions/prueba-tecnica-sf/user'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+  production: false,
+  dataUrl: 'https://my-json-server.typicode.com/HaibuSolutions/prueba-tecnica-sf/user'
+};
+
